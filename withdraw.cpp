ac2a561f741f6090d583817873d20d46908a4ba6
root <lionar037@yahoo.com.ar>
2017-12-04 02:29:26 -0300

    update

---
+++ withdraw.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"withdraw.h"
+
+using namespace std;
+Withdraw::Withdraw(){
+cout<<"constructor Withdraw"<<endl;
+}
+
+Withdraw::~Withdraw(){
+cout<<"destructor Withdraw"<<endl;
+}
+
---
+++ withdraw.h
@@ -0,0 +1,16 @@
+#ifndef __WITHDRAW_H__
+#define __WITHDRAW_H__
+
+
+class Withdraw
+{
+private:
+	int id_money;
+
+public:
+	Withdraw();
+	~Withdraw();
+
+};
+
+#endif
